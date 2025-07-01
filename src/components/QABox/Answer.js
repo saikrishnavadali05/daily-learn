c4bc99699700d201c92f1780ccad89e866fce4b5
saikrishnavadali05 <saikrishnavadali05@gmail.com>
2025-07-02 04:27:53 +0530

    fix answer box

--- src/components/QABox/Answer.js
+++ src/components/QABox/Answer.js
@@ -6,10 +6,10 @@ import { motion } from 'framer-motion';
 export default function Answer({ children }) {
   return (
     <motion.div
-      className={styles.questionBox}
+      className={styles.answerBox}
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
+      transition={{ duration: 1 }}
       >
         <strong>💡 Answer:</strong>
         <div>{children}</div>
